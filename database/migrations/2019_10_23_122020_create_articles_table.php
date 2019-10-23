12598de4e22f5f3c2725407f590a37ad4c785944
Li Enshun <lienshun@aliyun.com>
2019-10-23 20:51:34 +0800

    添加articles表

---
+++ database/migrations/2019_10_23_122020_create_articles_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedTinyInteger('status');
+            $table->string('title');
+            $table->string('preview');
+            $table->longText('content');
+            $table->unsignedBigInteger('author_id')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreign('author_id')->references('id')->on('users')->onDelete('set null');
+            $table->engine = 'InnoDB';
+            $table->charset = 'utf8mb4';
+            $table->collation = 'utf8mb4_unicode_ci';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
